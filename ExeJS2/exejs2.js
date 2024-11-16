a9d07184ca94a4f509263bcec43d44fcb4744ebf
Raffaele-Bervicato <rffbrv491@gmail.com>
2024-11-16 02:17:57 +0100

    inizio exeJS2

---
+++ ExeJS2/exejs2.js
@@ -0,0 +1,17 @@
+// TRACCIA 1
+
+let num1 = 3; let num2 = 5; let num3 = 7; let num4 = 9; let num5 = 11;
+
+let somma_numeri = num1 + num2 + num3 + num4 + num5;
+let media_numeri = (num1 + num2 + num3 + num4 + num5)/5
+
+console.log(`La somma tra i numeri equivale a ${ somma_numeri } e la media equivale a ${ media_numeri }`);
+
+// TRACCIA 2
+
+let input_età = prompt("Inserisci l'età");
+let fino_a_100 = 100 - (`${input_età}`);
+
+console.log(`anni per raggiungere i 100: ${fino_a_100}`);
+
+
